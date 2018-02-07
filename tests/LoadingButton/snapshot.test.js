b2458f791aa3631e423ad48ae5b023b40190017c
evilosa <bojkov@sorc.ru>
2018-02-07 23:25:02 +0300

    Fix tests.

--- tests/LoadingButton/snapshot.test.js
+++ tests/LoadingButton/snapshot.test.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import Button from '../../src/Button';
 
 const button = shallow(<Button />);
 
-it('renders correctly', () => {
-  expect(button).toMatchSnapshot();
-});
+describe('Button component', () => {
 
-test('render a label', () => {
-  const wrapper = shallow(
-    <div>Hello Jest!</div>
-  );
-  expect(wrapper).toMatchSnapshot();
+  it('should render without throwing an error', () => {
+    expect(shallow(<Button/>).exists(<button type="Button333">344</button>)).toBe(true);
+  });
+
+  it('renders correctly', () => {
+    expect(button).toMatchSnapshot();
+  });
+
+  it('should render to static HTML', function() {
+    expect(render(<Button>Her</Button>).text()).toEqual('Her');
+  });
+
+  test('render a label', () => {
+    const wrapper = shallow(
+      <div>Hello Jest!</div>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 });
