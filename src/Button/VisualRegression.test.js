16be386233471a11d296229c053890ddf82944fd
evilosa <bojkov@sorc.ru>
2018-02-09 16:33:15 +0300

    Update visual regression test with express server.

--- src/Button/VisualRegression.test.js
+++ src/Button/VisualRegression.test.js
@@ -5,17 +5,20 @@ expect.extend({ toMatchImageSnapshot });
 
 describe('Visual Regressions', () => {
   let chrome = null;
+  let server = null;
 
   beforeEach(() => {
+    server = require('../../scripts/server');
     chrome = new Chrome();
   });
 
   afterEach(() => {
     chrome.done();
+    server.close();
   });
 
   it('should NEVER happen', () => {
-    return chrome.goto('http://localhost:3000/buttons')
+    return chrome.goto('http://localhost:3000/')
       .then(() => chrome.screenshot())
       .then((image) => expect(image).toMatchImageSnapshot());
   });
