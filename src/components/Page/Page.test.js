d0588d1e2db8b9f456e38a8e83b5c913891d514f
evilosa <bojkov@sorc.ru>
2018-03-09 20:54:10 +0300

    Update tests for page.

--- src/components/Page/Page.test.js
+++ src/components/Page/Page.test.js
@@ -16,6 +16,39 @@ describe('Page', () => {
     expect(props.onClose).toBeUndefined()
   })
 
+  describe('with header', () => {
+    const page = shallow(
+      <Page
+        header='Test header'
+      >
+      </Page>
+    )
+
+    it('should render correct header', () => {
+      expect(page.contains(<h1>Test header</h1>)).toBeTruthy()
+    })
+  })
+
+  describe('onClose button', () => {
+    it('should render and be able to click if provided', () => {
+      const onClick = jest.fn();
+      const page = shallow(
+        <Page
+          onClose={onClick}
+        >
+        </Page>
+      )
+
+      page.find('button').simulate('click')
+      expect(onClick.mock.calls.length).toEqual(1)
+    })
+
+    it('should not be rendered if function is not provided', () => {
+      const page = shallow(<Page/>)
+      expect(page.find('button').length).toEqual(0)
+    })
+  })
+
   describe('with footer', () => {
     const page = shallow(
       <Page>
