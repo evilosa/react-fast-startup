ff88cf120507c72b35e6d9da128a8c868dbb81d1
evilosa <bojkov@sorc.ru>
2018-03-10 20:21:59 +0300

    Added first part of tests descriptions.

--- src/components/Select/Select.test.js
+++ src/components/Select/Select.test.js
@@ -15,4 +15,28 @@ describe('Select', () => {
     expect(state.options).toEqual([])
     expect(state.isOptionsVisible).toBeFalsy()
   })
+
+  describe('component select button', () => {
+    const component = shallow(<Select/>)
+
+    xit('should be visible if state.isOptionsVisible=false', () => {
+      expect(component.find('.select-btn-open').length).toEqual(1)
+    })
+    xit('should be invisible if state.isOptionsVisible=true')
+  })
+
+  describe('component refresh button', () => {
+    xit('should be visible if loadOptions function provided')
+    xit('should be visible if loadOptionsAsync function provided')
+    xit('should be invisible if loadOptions or loadOptionsAsync are undefined')
+    xit('should call handleRefresh when clicked')
+    xit('should call loadOptions or loadOptionsAsync if clicked')
+  })
+
+  describe('component clear button', () => {
+    xit('should be visible if value defined')
+    xit('should be invisible if value undefined')
+    xit('should call handleClear when clicked')
+    xit('should call onValueChange when clicked')
+  })
 })
